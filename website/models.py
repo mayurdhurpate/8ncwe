64aabf05004da65b0ee5a0b50e1c143b3674b723
Kushagrawal <kushagra.agrawal.civ14@itbhu.ac.in>
2016-09-13 23:11:05 +0530

    registration modified

--- website/models.py
+++ website/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class reguser(models.Model):
+    Name=models.CharField(max_length=100)
+    Designation= models.CharField(max_length=100)
+    Organisation = models.CharField(max_length=100)
+    Address= models.CharField(max_length=100)
+    Mob=models.CharField(max_length=15)
+    Email=models.CharField(max_length=100)
+    Membership=models.CharField(max_length=100)     
+    DD=models.CharField(max_length=100, blank=True)
+    Amount=models.CharField(max_length=100, blank=True)
+    Bank=models.CharField(max_length=15, blank=True)
+    Branch=models.CharField(max_length=100, blank=True)
+    Date=models.CharField(max_length=100, blank=True)
+    
+    
+    def __unicode__(self):
+        return "%s, %s" % (self.Name, self.Organisation)
+
+    def __str__(self):
+        return "%s, %s" % (self.Name, self.Organisation)
--- website/views.py
+++ website/views.py
@@ -14,3 +14,32 @@ def register(request):
 
 
 
+def regsubmit(request):
+    if request.method=='POST':
+        u=Reguser.objects.create(Name=request.POST['Name'],
+                                    Designation=request.POST['Designation'],
+                                    Organisation=request.POST['Organisation'],
+                                    Address= request.POST['Address']   ,
+                                    Mob=request.POST['Mob'],
+                                    Email=request.POST['Email'] ,
+                                    Membership=request.POST['Membership'],
+                                    DD=request.POST['DD'] ,
+                                    Amount=request.POST['Amount'] ,
+                                    Bank=request.POST['Bank'] ,
+                                    Branch=request.POST['Branch'] ,
+                                    Date=request.POST['Date'])
+
+
+      
+
+        
+        send_mail(
+            'Subject here',
+            'Thanks for submission. We have recorded your response.\nTitle of Paper'+u.Name+'\nName of 1st Author'+u.Mob+'\nCollege/university'
+            +u.college+'\ndesignation'+u.designation+'\nCo-Author'+u.coauthor+'\nPhone No.'+u.phone+'\nEmail'+u.email,
+            'no-reply@shilpiitbhu.org',
+            [u.email],
+            fail_silently=False,
+        )
+        return HttpResponse('Your response have been recorded.')
+    return HttpResponse('Authentication failed.')
